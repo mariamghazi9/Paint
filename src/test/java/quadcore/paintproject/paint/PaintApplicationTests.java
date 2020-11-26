5730f9f065cdb8bccefc1b41e8218bfccddfc1f1
Ahmad Waleed <a.waleedothman@gmail.com>
2020-11-26 18:23:24 +0200

    Initial commit

---
+++ src/test/java/quadcore/paintproject/paint/PaintApplicationTests.java
@@ -0,0 +1,13 @@
+package quadcore.paintproject.paint;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaintApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
