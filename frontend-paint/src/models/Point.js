9a5f3baa3546285624735ce227ab4e96f55a724e
asmaaramadan99 <58226647+asmaaramadan99@users.noreply.github.com>
2020-11-27 17:23:35 -0800

    point for triangle

---
+++ frontend-paint/src/models/Point.js
@@ -0,0 +1,16 @@
+class Point {
+    constructor(x,y)
+    { 
+        this.x=x;
+        this.y=y;
+
+    }
+
+     isInRange(contextWidth,contextHeight)
+    {
+       if(this.x>contextWidth || this.y>contextHeight) return false
+       if(this.x<0 || this.y<0) return false
+       return true
+    }
+}
+export default Point;
