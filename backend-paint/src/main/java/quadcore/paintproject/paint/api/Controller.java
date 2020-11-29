8659bc397ff8768923ca0c21f86d2ee47bcb669b
Mo'men Hamdy <53626982+Mo-hamdy2000@users.noreply.github.com>
2020-11-29 14:25:46 +0200

    data object recieving trial

--- backend-paint/src/main/java/quadcore/paintproject/paint/api/Controller.java
+++ backend-paint/src/main/java/quadcore/paintproject/paint/api/Controller.java
@@ -1,16 +1,34 @@
 package quadcore.paintproject.paint.api;
 
+import java.io.IOException;
+import java.util.Map;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 
+@CrossOrigin
 @RestController
 public class Controller {
-    @CrossOrigin(origins = "http://localhost:8081")
-    @RequestMapping("/calculator/{expression}")
-    public void index(@PathVariable String expression) {
-        //TODO
+    
+    @RequestMapping(value = "/addShape", method = RequestMethod.POST)
+    public String index(@RequestBody Map<String, Object> data) {
+        System.out.println(data.size());
+        System.out.println(data.get("type"));
+        System.out.println(data.get("shape"));
+        ObjectMapper mapper = new ObjectMapper();
+
+        try {
+            Map<String, String> map = mapper.readValue(data.get("shape").toString(), Map.class);
+            System.out.println(map);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return "hello";
     }
 }
